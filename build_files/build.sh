e476f1da9d2621b372a358e78f509657487912b8
Casper Thygesen <cth@trifork.com>
2025-09-10 22:28:35 +0200

    PINGGY_TOKEN

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,9 @@ set -ouex pipefail
 
 rsync -rvpK /ctx/system_files/ /
 
+mkdir -p /run/metadata/
+echo "PINGGY_TOKEN=$PINGGY_TOKEN" > /run/metadata/pinggy
+
 /ctx/build_files/github-cli.sh
 /ctx/build_files/server-docker-ce.sh
 /ctx/build_files/utilities.sh
