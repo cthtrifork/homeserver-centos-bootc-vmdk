e9715d0121fb4f407087e5e4dfe10020d4f2e9d3
Casper Thygesen <cth@trifork.com>
2025-09-10 22:36:45 +0200

    move to /etc/homeserver/metadata/pinggy

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 
 rsync -rvpK /ctx/system_files/ /
 
-mkdir -p /run/metadata/
-echo "PINGGY_TOKEN=$PINGGY_TOKEN" > /run/metadata/pinggy
+mkdir -p /etc/homeserver/metadata/
+echo "PINGGY_TOKEN=$PINGGY_TOKEN" > /etc/homeserver/metadata/pinggy
 
 /ctx/build_files/github-cli.sh
 /ctx/build_files/server-docker-ce.sh
