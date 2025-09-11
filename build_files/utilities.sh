8109242afb5eef9f0829b67616c94d94852dc66b
Casper Thygesen <cth@trifork.com>
2025-09-11 09:18:28 +0200

    install kubernetes utilities

--- build_files/utilities.sh
+++ build_files/utilities.sh
@@ -6,3 +6,18 @@ set -eou pipefail
 curl -Lo /tmp/age.tar.gz \
     "$(/ctx/build_files/github-release-url.sh FiloSottile/age linux-amd64.tar.gz)"
 tar -zxvf /tmp/age.tar.gz -C /usr/bin/ --strip-components=1 --exclude=LICENSE
+
+
+# kubectl
+curl -Lo /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 /tmp/kubectl /usr/bin/kubectl
+
+# kind
+curl -Lo /tmp/kind.tar.gz \
+    "$(/ctx/build_files/github-release-url.sh kubernetes-sigs/kind linux-amd64)"
+sudo install -o root -g root -m 0755 /tmp/kind /usr/bin/kind
+
+# fluxcd
+curl -Lo /tmp/flux.tar.gz \
+    "$(/ctx/build_files/github-release-url.sh fluxcd/flux2 linux-amd64.tar.gz)"
+tar -zxvf /tmp/flux.tar.gz -C /usr/bin/
