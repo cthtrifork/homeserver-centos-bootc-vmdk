dc1bf786238e400f1c6664848d59bde62cc7a1ce
Casper Thygesen <cth@trifork.com>
2025-09-11 10:07:32 +0200

    could not chmod

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,8 +8,7 @@ rsync -rvpK /ctx/system_files/ /
 mkdir -p /etc/homeserver/metadata/
 echo "PINGGY_TOKEN=$PINGGY_TOKEN" > /etc/homeserver/metadata/pinggy
 
-chmod -R +x /ctx/build_files/
-/ctx/build_files/github-cli.sh 
+/ctx/build_files/github-cli.sh
 /ctx/build_files/server-docker-ce.sh
 /ctx/build_files/utilities.sh
 /ctx/build_files/systemd.sh
