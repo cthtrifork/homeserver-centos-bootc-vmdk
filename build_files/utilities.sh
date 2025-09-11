594ff559496464ec7ccc13830df694bec9a95b4b
Casper Thygesen <cth@trifork.com>
2025-09-11 09:30:56 +0200

    debug statements

--- build_files/utilities.sh
+++ build_files/utilities.sh
@@ -9,15 +9,18 @@ tar -zxvf /tmp/age.tar.gz -C /usr/bin/ --strip-components=1 --exclude=LICENSE
 
 
 # kubectl
+echo "Installing kubectl"
 curl -Lo /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 /tmp/kubectl /usr/bin/kubectl
 
 # kind
+echo "Installing kind"
 curl -Lo /tmp/kind \
     "$(/ctx/build_files/github-release-url.sh kubernetes-sigs/kind linux-amd64)"
 install -o root -g root -m 0755 /tmp/kind /usr/bin/kind
 
 # fluxcd
+echo "Installing flux"
 curl -Lo /tmp/flux.tar.gz \
     "$(/ctx/build_files/github-release-url.sh fluxcd/flux2 linux-amd64.tar.gz)"
 tar -zxvf /tmp/flux.tar.gz -C /usr/bin/
