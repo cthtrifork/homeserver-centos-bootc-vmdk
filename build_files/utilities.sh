fe7e4bbe902daa7e122124b401e5401ce08c0066
Casper Thygesen <cth@trifork.com>
2025-09-11 13:59:07 +0200

    fix installations

--- build_files/utilities.sh
+++ build_files/utilities.sh
@@ -7,6 +7,9 @@ log() {
   echo "=== $* ==="
 }
 
+ARCH="amd64"
+MACHINE="linux"
+
 log "Installing age"
 curl -sLo /tmp/age.tar.gz \
     "$(/ctx/build_files/github-release-url.sh FiloSottile/age linux-amd64.tar.gz)"
@@ -15,6 +18,11 @@ tar -zxvf /tmp/age.tar.gz -C /usr/bin/ --strip-components=1 --exclude=LICENSE
 log "Installing kubectl"
 curl -sLo /tmp/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 /tmp/kubectl /usr/bin/kubectl
+
+log "Installing kubelogin"
+curl -sLo /tmp/kind \
+    "$(/ctx/build_files/github-release-url.sh int128/kubelogin ${MACHINE}_${ARCH}.zip)"
+tar -zxvf /tmp/flux.tar.gz -C /usr/bin/
 
 log "Installing kind"
 curl -sLo /tmp/kind \
@@ -27,9 +35,10 @@ curl -sLo /tmp/flux.tar.gz \
 tar -zxvf /tmp/flux.tar.gz -C /usr/bin/
 
 log "Installing k9s"
-curl -sLo /tmp/k9s.tar.gz \
-    "$(/ctx/build_files/github-release-url.sh derailed/k9s Linux_amd64.tar.gz)"
-tar -zxvf /tmp/k9s.tar.gz -C /usr/bin/ --exclude=LICENSE --exclude=README.md || file /tmp/k9s.tar.gz
+curl -sLo /tmp/k9s.rpm \
+    "$(/ctx/build_files/github-release-url.sh derailed/k9s Linux_amd64.rpm)"
+#tar -zxvf /tmp/k9s.tar.gz -C /usr/bin/ --exclude=LICENSE --exclude=README.md
+sudo dnf install /tmp/k9s.rpm
 
 log "Installing sops"
 curl -sLo /tmp/sops \
@@ -38,7 +47,7 @@ install -o root -g root -m 0755 /tmp/sops /usr/bin/sops
 
 log "Installing jq"
 curl -sLo /tmp/jq \
-    "$(/ctx/build_files/github-release-url.sh qlang/jq linux-amd64)"
+    "$(/ctx/build_files/github-release-url.sh jqlang/jq linux-amd64)"
 install -o root -g root -m 0755 /tmp/jq /usr/bin/jq
 
 log "Installing yq"
